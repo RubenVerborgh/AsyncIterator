b3fad5db6259482badc59b5526ac0d667702817d
jeswr <jesse.wright@anu.edu.au>
2021-07-20 23:49:47 +1000

    chore: Add type tests for filter function

---
+++ test/FilterTypeTest.ts
@@ -0,0 +1,22 @@
+import { ArrayIterator } from '../asynciterator';
+import type { AsyncIterator } from '../asynciterator';
+
+const iterator = new ArrayIterator<string | number>([1, 'h', 4, 5, 7, 's']);
+
+const filteredIteratorReturnIs: AsyncIterator<string> = iterator.filter<string>(
+  (item): item is string => typeof item === 'string'
+);
+
+const filteredIteratorReturnBool: AsyncIterator<string | number> = iterator.filter(
+  (item): boolean => typeof item === 'string'
+);
+
+const filteredIteratorReturnTrue: AsyncIterator<string | number> = iterator.filter(
+  () => true
+);
+
+export {
+  filteredIteratorReturnIs,
+  filteredIteratorReturnBool,
+  filteredIteratorReturnTrue,
+};
